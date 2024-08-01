d88bec34d7ce9264456eac8239bc1c93901a84c6
Bartek <bar.zaborowski@gmail.com>
2024-08-01 22:40:13 +0200

    configure app

--- Personal.WWW/src/app/app.component.ts
+++ Personal.WWW/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppMenuComponent } from "../core/components/app-menu/app-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Personal.WWW';
+  title = 'PersonalStats';
 }
