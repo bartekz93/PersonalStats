41876e64d46d3e1c9b961d4cdeaafe46bd03e31d
Bartek <bar.zaborowski@gmail.com>
2024-08-09 02:44:51 +0200

    changed native form to AppForm in AppFilterPanel

--- Personal.WWW/src/core/components/app-filter-panel/app-filter-panel.component.ts
+++ Personal.WWW/src/core/components/app-filter-panel/app-filter-panel.component.ts
@@ -1,21 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { TranslateModule } from '@ngx-translate/core';
 import { AppButtonComponent } from '../app-button/app-button.component';
+import { AppFormComponent } from '../app-form/app-form.component';
 
 @Component({
     standalone: true,
     selector: 'app-filter-panel',
     templateUrl: 'app-filter-panel.component.html',
-    imports: [CardModule, CommonModule, FormsModule, TranslateModule, AppButtonComponent]
+    imports: [CardModule, CommonModule, FormsModule, TranslateModule, AppButtonComponent, AppFormComponent]
 })
 
 export class AppFilterPanel<T> implements OnInit {
     constructor() { }
 
     @Input() loading = false;
+    @Input() formGroup!: FormGroup;
 
     @Output() onSearch: EventEmitter<void> = new EventEmitter();
     @Output() onClear: EventEmitter<void> = new EventEmitter();
