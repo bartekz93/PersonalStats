7cfa389fd1bca6e98b391f480d9a861b91849694
Bartek <bar.zaborowski@gmail.com>
2024-08-12 17:29:59 +0200

    add string formatter with parameters

--- Personal.WWW/src/core/helpers/formatters.ts
+++ Personal.WWW/src/core/helpers/formatters.ts
@@ -3,5 +3,13 @@ const decimalSep = ',';
 export const Format = {
     decimal(v: number, decPlaces: number = 2) {
         return Number(v).toFixed(decPlaces).replace('.', decimalSep);
+    },
+    string(text: string, params: any) {
+        if (!params) return text;
+        for (let key of Object.keys(params)) {
+            text = text.replace(new RegExp("{" + key + "}", "gi"), params[key]);
+        }
+        return text;
     }
+
 }
