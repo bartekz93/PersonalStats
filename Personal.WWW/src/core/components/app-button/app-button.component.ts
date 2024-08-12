034219d7a51297af364f0a4ad7043bce9ed20702
Bartek <bar.zaborowski@gmail.com>
2024-08-12 17:28:48 +0200

    add round style and tooltip to AppButton

--- Personal.WWW/src/core/components/app-button/app-button.component.ts
+++ Personal.WWW/src/core/components/app-button/app-button.component.ts
@@ -1,19 +1,25 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
+import { Tooltip, TooltipModule } from 'primeng/tooltip';
 
 @Component({
     standalone: true,
-    imports: [ TranslateModule, ButtonModule ],
+    imports: [ TranslateModule, ButtonModule, TooltipModule ],
     selector: 'app-button',
     template: `
         <p-button 
+            [style]="round ? { width: '40px', height: '40px' } : {}"
             (onClick)="click()" 
             [type]="submit ? 'submit' : 'button'" 
             [loading]="loading" 
             [icon]="icon" 
+            [rounded]="round"
             size="small"
-            [outlined]="flat"
+            [text]="flat"
+            [pTooltip]="tooltip | translate" 
+            tooltipPosition="bottom" 
+            showDelay="500" 
             [severity]="secondary ? 'secondary' : 'primary'"
             [label]="label | translate" />
         `
@@ -22,11 +28,13 @@ import { ButtonModule } from 'primeng/button';
 export class AppButtonComponent implements OnInit {
     constructor() { }
 
+    @Input() tooltip = '';
     @Input() loading = false;
     @Input() icon = '';
     @Input() label = '';
     @Input() secondary = false;
     @Input() flat = false;
+    @Input() round = false;
     @Input() submit = false;
 
     @Output() onClick = new EventEmitter();
