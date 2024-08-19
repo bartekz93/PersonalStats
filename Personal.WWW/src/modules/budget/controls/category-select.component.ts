53cdf063273ef65265dcdc3a41b9fc6f38a284ed
Bartek <bar.zaborowski@gmail.com>
2024-08-19 18:07:13 +0200

    CategorySelect and WalletSelect components

--- Personal.WWW/src/modules/budget/controls/category-select.component.ts
+++ Personal.WWW/src/modules/budget/controls/category-select.component.ts
@@ -2,24 +2,24 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { CategoryService } from '@budget/services/category.service';
 import { AppCircleLabelComponent } from '@core/components/app-circle-label/app-circle-label.component';
-import { AppMultiSelectComponent } from '@core/controls/app-multi-select.component';
+import { AppSelectComponent } from '@core/controls/app-select.component';
 import { AppTemplateDirective } from '@core/directives/app-template.directive';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     standalone: true,
-    imports: [ AppMultiSelectComponent, AppCircleLabelComponent, AppTemplateDirective, TranslateModule ],
-    selector: 'category-multi-select',
+    imports: [ AppSelectComponent, AppCircleLabelComponent, AppTemplateDirective, TranslateModule ],
+    selector: 'category-select',
     template: `
-        <app-multi-select [options]="options" [label]="label" [fc]="fc" >
+        <app-select [filter]="true" [options]="options" [label]="label" [fc]="fc" >
             <ng-template appTemplate="item" let-item="item">
                 <app-circle-label [icon]="item.icon" [color]="item.color" [label]="item.label | translate" [size]="1"/>
             </ng-template>
-        </app-multi-select>
+        </app-select>
     `
 })
 
-export class CategoryMultiSelectComponent implements OnInit {
+export class CategorySelectComponent implements OnInit {
     constructor(private categoryService: CategoryService) {
     }
 
--- Personal.WWW/src/modules/budget/controls/wallet-select.component.ts
+++ Personal.WWW/src/modules/budget/controls/wallet-select.component.ts
@@ -1,26 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { CategoryService } from '@budget/services/category.service';
+import { WalletService } from '@budget/services/wallets.service';
 import { AppCircleLabelComponent } from '@core/components/app-circle-label/app-circle-label.component';
-import { AppMultiSelectComponent } from '@core/controls/app-multi-select.component';
+import { AppSelectComponent } from '@core/controls/app-select.component';
 import { AppTemplateDirective } from '@core/directives/app-template.directive';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     standalone: true,
-    imports: [ AppMultiSelectComponent, AppCircleLabelComponent, AppTemplateDirective, TranslateModule ],
-    selector: 'category-multi-select',
+    imports: [ AppSelectComponent, AppCircleLabelComponent, AppTemplateDirective, TranslateModule ],
+    selector: 'wallet-select',
     template: `
-        <app-multi-select [options]="options" [label]="label" [fc]="fc" >
+        <app-select [options]="options" [label]="label" [fc]="fc" >
             <ng-template appTemplate="item" let-item="item">
                 <app-circle-label [icon]="item.icon" [color]="item.color" [label]="item.label | translate" [size]="1"/>
             </ng-template>
-        </app-multi-select>
+        </app-select>
     `
 })
 
-export class CategoryMultiSelectComponent implements OnInit {
-    constructor(private categoryService: CategoryService) {
+export class WalletSelectComponent implements OnInit {
+    constructor(private walletService: WalletService) {
     }
 
     @Input() fc!: FormControl;
@@ -29,12 +29,12 @@ export class CategoryMultiSelectComponent implements OnInit {
     options: any[] = [];
 
     async ngOnInit() {
-        let categories = await this.categoryService.search({});
-        this.options = categories.rows.map(x => ({
-            label: x.name,
-            value: x.id,
-            icon: x.icon,
-            color: x.color
+        let wallets = await this.walletService.search({});
+        this.options = wallets.rows.map(w => ({
+            label: w.name,
+            value: w.id,
+            icon: 'pi pi-wallet',
+            color: w.color
         }))
     }
 }
